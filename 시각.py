05012ec9543751aa3a7887e84ea42d3c1ea326c1
jsh9611 <jangsh9611@naver.com>
2021-02-09 23:45:42 +0900

    구현

---
+++ 시각.py
@@ -0,0 +1,12 @@
+#시각.py
+
+h = int(input())
+
+count = 0
+for i in range(h+1):
+    for j in range(60):
+        for k in range(60):
+            # print(str(i)+str(j)+str(k))
+            if '3' in str(i)+str(j)+str(k):
+                count += 1
+print(count)
