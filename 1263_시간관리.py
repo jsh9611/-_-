c3fe97b80213bb0c7b913816441029cb7a35c50a
jsh9611 <jangsh9611@naver.com>
2021-08-02 20:12:10 +0900

    코멘트 추가

--- 1263_시간관리.py
+++ 1263_시간관리.py
@@ -5,26 +5,21 @@ time = []
 for i in range(n):
     time.append(list(map(int, sys.stdin.readline().split())))
 scd = sorted(time, key=lambda x: -x[1])  
-print(scd)
 
+# 0번째 끝나는 시간으로 ans 초기화
 ans = scd[0][1]-scd[0][0]
 
 for i in range(1,n):
+    # 끝내야 하는 시간이 현재 시간보다 앞선 경우
     if ans>scd[i][1]:
+        # i번째 일의 끝나는 시간을 기준으로 일한 만큼 빼서 갱신
         ans = scd[i][1]-scd[i][0]
+    #  끝내야 하는 시간이 현재 시간보다 나중인 경우
     else:
+        # 현재시간을 기준으로 일한 만큼 빼서 갱신
         ans = ans - scd[i][0]
 print(ans if ans>=0 else -1)
 
-    # else:
-    #     print(scd[0][1]-scd[0][0])
-
-    # if scd[0][1]-scd[0][0]<left:
-    #     print(scd[0][1]-scd[0][0])
-    # elif left < 0:
-    #     print(-1)
-    # else:
-    #     print(left)
 """
 4
 3 5
@@ -50,20 +45,4 @@ print(ans if ans>=0 else -1)
 2 2
 3 3
 =>-1
-
-
 """
-# import sys
-# N = int(input())
-# arr = []
-# for _ in range(N):
-#     a, b = map(int, sys.stdin.readline().split())
-#     arr.append((b, a))
-# # 가장 늦은 시간에 해결해도 되는 일 순으로 역순 정렬
-# arr.sort(reverse=True)
-# print(arr)
-
-# 15
-# 14
-# 6
-# 3
